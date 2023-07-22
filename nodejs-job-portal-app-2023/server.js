c8c9c83bec973747e1bd100e71f5690f37c5bfdf
Arun <123rjk04@gmail.com>
2023-07-22 18:53:04 +0530

    first time commit

---
+++ nodejs-job-portal-app-2023/server.js
@@ -0,0 +1,23 @@
+// imports -----
+import express from "express";
+import dotenv from "dotenv"; // isko import kerne ke bad configration bhi kerna padta hai, tabhi hm is .env mtlb environmental variable ka use ker payegay
+import colors from "colors";
+
+// DOT ENV config - configration hame starting me kuch is tarah se kerna hoga
+dotenv.config();
+
+// Rest Object
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to Job Portal...</h1>")
+})
+
+// Port
+const PORT = process.env.PORT || 8080; 
+
+// Listen
+app.listen(PORT, () => {
+    console.log(`Node Server Running in ${process.env.DEV_MODE} Mode on Port no. ${PORT}`.bgBlue.white)
+})
